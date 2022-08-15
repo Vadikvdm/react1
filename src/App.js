aeedc707ed3c3d84ae00ffba7935b7ed0ae70407
Vadikvdm <vadikvdm@gmail.com>
2022-08-15 11:36:34 +0300

    modifi my app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Posts from './Component/Posts/Posts';
+
+const ANAKIN_IMAGE = 'https://i1.sndcdn.com/avatars-000560486658-t1txpn-t240x240.jpg';
+const RAY_IMAGE = 'https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fon'>
+          <div className="App">
+            <Posts
+        author={{
+          name: "Anakin Skywalker",
+          photo: ANAKIN_IMAGE,
+          nickname: "@dart_vader",
+        }}
+        content="WTF?  Who is Ray?  Why she is Skywalker?  Luke...?"
+        image={RAY_IMAGE}
+        date={" 26 лютого."}
+      />
+    </div>
     </div>
   );
 }
